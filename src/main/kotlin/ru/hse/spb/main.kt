33d2bd9cc2a7b04ac713e0d2c7cbba32b4a549ef
lergor <gor.ler177@gmail.com>
2018-09-11 23:34:44 +0300

    complete task1

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,71 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+// problem 51B: http://codeforces.com/contest/51/submission/42777201
 
-    return words.joinToString(separator = " ")
+class TableParser {
+
+    private var listOfSizes = mutableListOf<Int>()
+    private var string = ""
+
+    fun getTableSizes(text: String): List<Int> {
+        listOfSizes = mutableListOf()
+        string = text.replace("\n", "")
+        parseTable()
+        return listOfSizes.sorted()
+    }
+
+    private fun parseTable() {
+        var cells = 0
+        while (string.isNotEmpty()) {
+            when {
+                string.startsWith("<table>") -> {
+                    removeTag("<table>")
+                    parseTable()
+                }
+                string.startsWith("</table>") -> {
+                    listOfSizes.add(cells)
+                    removeTag("</table>")
+                    return
+                }
+                string.startsWith("<td>") -> {
+                    removeTag("<td>")
+                    cells++
+                }
+                string.startsWith("<tr>") -> {
+                    removeTag("<tr>")
+                }
+                string.startsWith("</tr>") -> {
+                    removeTag("</tr>")
+                }
+                string.startsWith("</td>") -> {
+                    removeTag("</td>")
+                }
+                else -> return
+            }
+        }
+    }
+
+    private fun removeTag(tag: String) {
+        string = string.removePrefix(tag)
+    }
+}
+
+private fun readTable(): String {
+    var table = ""
+    var line = readLine()
+    while (!line.isNullOrBlank()) {
+        table += line
+        line = readLine()
+    }
+    return table
+}
+
+private fun run() {
+    val parser = TableParser()
+    val listOfSizes = parser.getTableSizes(readTable())
+    println(listOfSizes.joinToString(separator = " "))
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    run()
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -4,8 +4,44 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class TestSource {
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun test1() {
+        val string = "<table><tr><td></td></tr></table>"
+        val parser = TableParser()
+        val listOfSizes = parser.getTableSizes(string)
+        assertEquals(listOf(1), listOfSizes)
+    }
+
+    @Test
+    fun test2() {
+        val string = "<table>\n" +
+                "<tr>\n" +
+                "<td>\n" +
+                "<table><tr><td></td></tr><tr><td></\n" +
+                "td\n" +
+                "></tr><tr\n" +
+                "><td></td></tr><tr><td></td></tr></table>\n" +
+                "</td>\n" +
+                "</tr>\n" +
+                "</table>"
+        val parser = TableParser()
+        val listOfSizes = parser.getTableSizes(string)
+        assertEquals(listOf(1, 4), listOfSizes)
+    }
+
+    @Test
+    fun test3() {
+        val string = "<table><tr><td>\n" +
+                "<table><tr><td>\n" +
+                "<table><tr><td>\n" +
+                "<table><tr><td></td><td></td>\n" +
+                "</tr><tr><td></td></tr></table>\n" +
+                "</td></tr></table>\n" +
+                "</td></tr></table>\n" +
+                "</td></tr></table>"
+        val parser = TableParser()
+        val listOfSizes = parser.getTableSizes(string)
+        assertEquals(listOf(1, 1, 1, 3), listOfSizes)
     }
 }
